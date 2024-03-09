a5815518dc6ef0c09a5adbf2895f345ec0b47821
Yohan Thibault <y2thibaultgm@gmail.com>
2024-03-09 16:40:41 -0500

    Commit initial

---
+++ src/test/java/claurendeau/hackqc/algo/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package claurendeau.hackqc.algo.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
