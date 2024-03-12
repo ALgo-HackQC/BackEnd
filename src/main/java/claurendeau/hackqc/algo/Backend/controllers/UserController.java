2fd95b17bdb9aafab4c694968f43849955137a67
Yohan Thibault <y2thibaultgm@gmail.com>
2024-03-12 13:51:30 -0400

    Cree utilisateur : ajout UserCreatorDTO

--- src/main/java/claurendeau/hackqc/algo/Backend/controllers/UserController.java
+++ src/main/java/claurendeau/hackqc/algo/Backend/controllers/UserController.java
@@ -1,7 +1,9 @@
 package claurendeau.hackqc.algo.Backend.controllers;
 
+import claurendeau.hackqc.algo.Backend.dto.UserCreatorDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/utilisateur")
 public class UserController {
     @PostMapping("/cree")
-    public ResponseEntity<String> createUser() {
+    public ResponseEntity<String> createUser(@RequestBody UserCreatorDTO userCreatorDTO) {
+
+
         return null;
     }
 }
