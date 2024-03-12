61840bec20bbb6ae52279e393db23a16da7f00fd
Yohan Thibault <y2thibaultgm@gmail.com>
2024-03-12 16:32:28 -0400

    Test cree utilisateur : Ajout test normal

--- src/test/java/claurendeau/hackqc/algo/Backend/service/UserServiceTest.java
+++ src/test/java/claurendeau/hackqc/algo/Backend/service/UserServiceTest.java
@@ -1,2 +1,35 @@
-package claurendeau.hackqc.algo.Backend.service;public class UserServiceTest {
+package claurendeau.hackqc.algo.Backend.service;
+
+import claurendeau.hackqc.algo.Backend.dto.UserDTO;
+import claurendeau.hackqc.algo.Backend.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    UserService userService;
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    public void test_createUser_normal() {
+        String lastName = "Lastname";
+        String firstName = "Firstname";
+        String email = "email@email.com";
+        String password = "test";
+
+        UserDTO userDTO = userService.createUser(lastName, firstName, email, password);
+
+        assertTrue(userRepository.existsById(userDTO.id()));
+
+        assertEquals(lastName, userDTO.lastName());
+        assertEquals(firstName, userDTO.firstName());
+        assertEquals(email, userDTO.email());
+    }
+
 }
