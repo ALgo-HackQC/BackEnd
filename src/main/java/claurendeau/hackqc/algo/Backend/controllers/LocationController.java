32586ce59973ff71f6179760ad9a5f330c53ebd3
Emanuel Paige <2143602@claurendeau.qc.ca>
2024-03-13 12:05:56 -0400

    Return response entity

--- src/main/java/claurendeau/hackqc/algo/Backend/controllers/LocationController.java
+++ src/main/java/claurendeau/hackqc/algo/Backend/controllers/LocationController.java
@@ -1,6 +1,7 @@
 package claurendeau.hackqc.algo.Backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,8 @@ public class LocationController {
     LocationService locationService;
 
     @GetMapping("/{id}")
-    public String getLocationById(@PathVariable Long id) {
-        return locationService.getLocationById(id).toString();
+    public ResponseEntity<String> getLocationById(@PathVariable Long id) {
+        String location = locationService.getLocationById(id).toString();
+        return new ResponseEntity<>(location, location == null ? org.springframework.http.HttpStatus.NOT_FOUND : org.springframework.http.HttpStatus.OK);
     }
 }
