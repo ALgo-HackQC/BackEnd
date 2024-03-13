120e1c1fcf7e43b5b750e92cf28c90de4cd7dcaa
Yohan Thibault <y2thibaultgm@gmail.com>
2024-03-13 12:33:41 -0400

    Connexion utilisateur : Ajout ConnectionRepository

---
+++ src/main/java/claurendeau/hackqc/algo/Backend/repository/ConnectionRepository.java
@@ -0,0 +1,7 @@
+package claurendeau.hackqc.algo.Backend.repository;
+
+import claurendeau.hackqc.algo.Backend.modeles.Connection;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConnectionRepository extends JpaRepository<Connection, Long> {
+}
